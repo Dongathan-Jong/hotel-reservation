f16fcb6a92c8216030b996ac90229f8a14edc780
Dongathan-Jong <duclingsyt@gmail.com>
2024-08-17 20:46:38 -0400

    Update Main5.java

--- Main5.java
+++ Main5.java
@@ -37,11 +37,72 @@ public class Main5 {
          option = sc.nextLine();
          menuOption = false;
 
-         
+         while (!menuOption) {
+            if (option.trim().equalsIgnoreCase("log out")) {
+               adminOrEmployee = loginPortal();
+               menuOption = true;
+            } else if (option.equals("1")) {
+               fullDate = askForDate();
+               availableRoomsPerDate(fullDate);
+               menuOption = true;
+            } else if (option.trim().equals("2")) {
+               fullDate = askForDate();
+               allReservationsPerDate(fullDate);
+               menuOption = true;
+            } else if (option.trim().equals("3")) {
+               allReservationsPerName();
+               menuOption = true;
+            } else if (option.trim().equals("4")) {
+               bookingReservations();
+               menuOption = true;
+            } else if (option.trim().equals("5")) {
+            } else if (option.trim().equals("6")) {
+            } else if (option.trim().equals("7")) {
+            } else if (option.trim().equals("8")) {
+            } else if (option.trim().equals("9")) {
+            } else if (adminOrEmployee == 0) {
+               if (option.trim().equals("10")) {
+                  System.out.println("option 10!!!! ");
+                  menuOption = true;
+               }
+            } else {
+               System.out.print("Not an option. Try again.\n");
+               menuOption = false;
+               System.out.print("Enter option: ");
+               option = sc.nextLine();
+            }
+         }
+      }
    }
 
    public static int loginPortal() {
-      
+      Scanner sc = new Scanner(System.in);
+      boolean employeeLoginPortal = false;
+      int returnValueInt = 1;
+      String employeeNumberInputString = "";
+      int goodEmployeeNumber = 1;
+      String employeePinNumberInputString;
+      int employeeNumAndPinMatch = 1;
+      System.out.println("WELCOME TO LOG IN PORTAL.");
+
+      while (!employeeLoginPortal) {
+         while (goodEmployeeNumber == 1) {
+            System.out.print("\nEnter your Employee Number: ");
+            employeeNumberInputString = sc.nextLine();
+            goodEmployeeNumber = checkEmployeeNumber(employeeNumberInputString);
+
+            if (goodEmployeeNumber == 0) {
+               returnValueInt = 0;
+            } else if (goodEmployeeNumber == 2) {
+               returnValueInt = 2;
+            }
+         }
+
+         if (employeeNumAndPinMatch == 0) {
+            employeeNumAndPinMatch = 1;
+         }
+
+         
    }
 
    public static int checkEmployeeNumber(String employeeNumberInputString) {
@@ -58,4 +119,17 @@ public class Main5 {
 
    public static void availableRoomsPerDate(String fullDate) {
       
+   }
+
+   public static void allReservationsPerDate(String fullDate) {
+      
+   }
+
+   public static void allReservationsPerName() {
+      
+   }
+
+   public static void bookingReservations() {
+      
+   }
 }
